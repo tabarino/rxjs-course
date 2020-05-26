29a91b267968ae1cfa2b6ded8246f6dff8e340b0
tabarino <tabarino@outlook.com>
2020-05-26 23:54:19 +0100

    Sharing HTTP Responses with the shareReplay Operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,7 +20,9 @@ export class HomeComponent implements OnInit {
         const http$ = createHttpObservable('/api/courses');
         const courses$: Observable<Course[]> = http$
             .pipe(
-                map(res => Object.values(res['payload']))
+                tap(() => console.log('HTTP Request executed')),
+                map(res => Object.values(res['payload'])),
+                shareReplay()
             );
 
         this.beginnersCourses$ = courses$
